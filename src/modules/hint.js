888aeb2042cc6ff4f192163eec1ed00c7e8f876a
Vasily <vasily-bazhenov.03@mail.ru>
2022-05-05 13:58:53 +0300

    feat: rotate hint

--- src/modules/hint.js
+++ src/modules/hint.js
@@ -5,11 +5,17 @@ const hint = () => {
     hintLink.forEach((link, index) => {
         link.addEventListener('mouseenter', () => {
             hintModal[index].style.visibility = 'visible'
-            console.log('wfew')
+            hintModal[index].style.opacity = 1
+            console.log(hintModal[index].getBoundingClientRect().top);
+            if (hintModal[index].getBoundingClientRect().top <= 0) {
+                hintModal[index].style.transform = 'rotate(180deg)'
+                hintModal[index].style.transform = 'rotate(180deg)'
+            }
         })
-        // link.addEventListener('mouseover', () => {
-        //     hintModal[index].style.visibility = 'hidden'
-        // })
+        link.addEventListener('mouseout', () => {
+            hintModal[index].style.visibility = 'hidden'
+            hintModal[index].style.opacity = 0.1
+        })
     })
 }
 
